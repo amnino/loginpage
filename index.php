faa1aa98ad9cfa7a5991355a5d3720597b599d2e
amnino <34503795+amnino@users.noreply.github.com>
2018-05-16 09:24:35 +0545

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<title>Login System</title>
+	<meta name="viewport" content="widht=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="first.css">
+	<body>
+	<div class="container">
+				<h2>Admin Login</h2>
+				<img class="logo" src="logo.png" height="120px" width="120px" align="center"></img>
+				<form class='my-form' method="POST">
+						<div class="form-group">
+									<input type="text" name="uid" placeholder="USERNAME"><br>
+						</div>
+						<div class="form-group">
+								<input type="password" name="pwd" placeholder="PASSWORD"><br>
+						</div>
+
+								<input class="button" type="submit" name="submit" value="LOGIN">
+						<p>
+							No access?<br>
+							<a href="contact.php">administration</a>
+						</p>
+		</form>
+	</div>
+	</body>
+</html>
